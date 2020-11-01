92af0aed27b471a122ea7085e521a3ddce2f808e
belbitexno <acubensby@users.noreply.github.com>
2020-11-01 17:50:37 +0300

    added hm2

--- src/Gena/Cheak.java
+++
@@ -1,7 +0,0 @@
-package Gena;
-
-public class Cheak {
-    public static void main(String[] args) {
-        System.out.println("cheack");
-    }
-}
---
+++ src/hm2/Test.java
@@ -0,0 +1,7 @@
+package hm2;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("jjjj");
+    }
+}
